ef455ba74e6d625ec70c781480f9d29d77e786d0
BenSymons <bsymonsb@hotmail.co.uk>
2021-03-18 11:31:32 +0000

    create loading pattern and list of people

--- src/components/people.jsx
+++ src/components/people.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 
 const People = () => {
     const [list, setList] = useState("none")
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         getData()
@@ -10,12 +11,25 @@ const People = () => {
     const getData = () => {
         fetch("https://8ee41f94-d4f4-439d-8233-e573edca74ff.mock.pstmn.io/users")
             .then(res => res.json())
-            .then(data => setList(data))
+            .then(data => {
+                setList(data)
+                setLoading(false)
+            })
     }
-    console.dir(list)
+    console.log(loading)
+    console.log(list)
+    if (loading) return <p>loading...</p>
     return (
-        <p>list goes here</p>
+        <div>
+            <ul>
+                {list.data.map(person => {
+                    return (<li>
+                        <p>{person.name}</p>
+                        <p>{person.email}</p>
+                    </li>)
+                })}
+            </ul>
+        </div>
     )
 }
-
 export default People
